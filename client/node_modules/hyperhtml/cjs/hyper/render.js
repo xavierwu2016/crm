f6ebce5c222aebd5d595fad9267476a2faea42cd
Xavier Wu <shield18@xavierwu.com>
2019-11-30 18:22:05 +0800

    Initial commit

---
+++ client/node_modules/hyperhtml/cjs/hyper/render.js
@@ -0,0 +1,39 @@
+'use strict';
+const WeakMap = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('@ungap/weakmap'));
+const tta = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('@ungap/template-tag-arguments'));
+
+const {OWNER_SVG_ELEMENT} = require('../shared/constants.js');
+const {Tagger} = require('../objects/Updates.js');
+
+// a weak collection of contexts that
+// are already known to hyperHTML
+const bewitched = new WeakMap;
+
+// better known as hyper.bind(node), the render is
+// the main tag function in charge of fully upgrading
+// or simply updating, contexts used as hyperHTML targets.
+// The `this` context is either a regular DOM node or a fragment.
+function render() {
+  const wicked = bewitched.get(this);
+  const args = tta.apply(null, arguments);
+  if (wicked && wicked.template === args[0]) {
+    wicked.tagger.apply(null, args);
+  } else {
+    upgrade.apply(this, args);
+  }
+  return this;
+}
+
+// an upgrade is in charge of collecting template info,
+// parse it once, if unknown, to map all interpolations
+// as single DOM callbacks, relate such template
+// to the current context, and render it after cleaning the context up
+function upgrade(template) {
+  const type = OWNER_SVG_ELEMENT in this ? 'svg' : 'html';
+  const tagger = new Tagger(type);
+  bewitched.set(this, {tagger, template: template});
+  this.textContent = '';
+  this.appendChild(tagger.apply(null, arguments));
+}
+
+Object.defineProperty(exports, '__esModule', {value: true}).default = render;
---
+++ client/node_modules/hyperhtml/cjs/shared/constants.js
@@ -0,0 +1,18 @@
+'use strict';
+// Node.CONSTANTS
+// 'cause some engine has no global Node defined
+// (i.e. Node, NativeScript, basicHTML ... )
+const ELEMENT_NODE = 1;
+exports.ELEMENT_NODE = ELEMENT_NODE;
+const DOCUMENT_FRAGMENT_NODE = 11;
+exports.DOCUMENT_FRAGMENT_NODE = DOCUMENT_FRAGMENT_NODE;
+
+// SVG related constants
+const OWNER_SVG_ELEMENT = 'ownerSVGElement';
+exports.OWNER_SVG_ELEMENT = OWNER_SVG_ELEMENT;
+
+// Custom Elements / MutationObserver constants
+const CONNECTED = 'connected';
+exports.CONNECTED = CONNECTED;
+const DISCONNECTED = 'dis' + CONNECTED;
+exports.DISCONNECTED = DISCONNECTED;
